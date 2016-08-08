a132bfcd7e200e093eff0e6cd165402acfd97bc4
loveKino <842914439@qq.com>
2016-08-08 17:39:25 +0800

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * code case
+ */
+
+let {
+    n, view
+} = require('kabanery');
+
+var ace = require('brace');
+require('brace/mode/javascript');
+require('brace/theme/monokai');
+
+let count = 0;
+
+/**
+ * {
+ *  id,
+ *  content
+ * }
+ */
+module.exports = view((data) => {
+    data.id = data.id || `editor-code-${count++}`;
+    let editor = null;
+
+    setTimeout(() => {
+        editor = ace.edit(data.id);
+        editor.getSession().setMode('ace/mode/javascript');
+        editor.setTheme('ace/theme/monokai');
+        editor.setValue(data.content || '');
+        editor.clearSelection();
+    }, 0);
+
+    return n(`div id=${data.id}`, {
+        style: {
+            width: '100%',
+            height: '100%'
+        },
+        onkeyup: () => {
+            data.content = editor.getValue();
+        }
+    });
+});
