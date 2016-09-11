f1938886fd754fcd23dc8a59c7ffb3f6a5780622
loveKino <842914439@qq.com>
2016-09-12 03:19:35 +0800

    bug fix

--- index.js
+++ index.js
@@ -25,7 +25,15 @@ module.exports = view((data) => {
     data.id = data.id || `editor-code-${count++}`;
     let editor = null;
 
+    // TODO opt
     setTimeout(() => {
+        let curNode = document.getElementById(data.id);
+        let parent = curNode.parentNode;
+        parent.removeChild(curNode);
+        let newChild = document.createElement('div');
+        newChild.id = data.id;
+        newChild.style = 'width:100%;height:100%';
+        parent.appendChild(newChild);
         editor = ace.edit(data.id);
         editor.getSession().setMode('ace/mode/javascript');
         editor.setTheme('ace/theme/monokai');
@@ -33,14 +41,21 @@ module.exports = view((data) => {
         editor.clearSelection();
     }, 0);
 
-    return n(`div id=${data.id}`, {
+    return n('div', {
         style: {
             width: '100%',
             height: '100%'
-        },
-        onkeyup: () => {
-            data.content = editor.getValue();
-            data.onchange && data.onchange(data.content);
         }
-    });
+    }, [
+        n(`div id=${data.id}`, {
+            style: {
+                width: '100%',
+                height: '100%'
+            },
+            onkeyup: () => {
+                data.content = editor.getValue();
+                data.onchange && data.onchange(data.content);
+            }
+        })
+    ]);
 });
